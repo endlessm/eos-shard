78aa6597d6b1f43b8ab8af492fd40569d2123076
Will Greenberg <will@endlessm.com>
2015-05-04 10:45:17 -0700

    Correct fd handling

--- src/epak-pak.c
+++ src/epak-pak.c
@@ -50,7 +50,6 @@ epak_pak_init_internal (GInitable *initable,
 
   priv->fd = open (priv->path, O_RDONLY);
   if (priv->fd < 0) {
-    close (priv->fd);
     return FALSE;
   }
 
@@ -123,20 +122,10 @@ epak_pak_finalize (GObject *object)
   EpakPak *pak = EPAK_PAK (object);
   EpakPakPrivate *priv = epak_pak_get_instance_private (pak);
 
+  close (priv->fd);
   g_clear_pointer (&priv->path, g_free);
 
   G_OBJECT_CLASS (epak_pak_parent_class)->finalize (object);
-}
-
-static void
-epak_pak_dispose (GObject *object)
-{
-  EpakPak *pak = EPAK_PAK (object);
-  EpakPakPrivate *priv = epak_pak_get_instance_private (pak);
-
-  close (priv->fd);
-
-  G_OBJECT_CLASS (epak_pak_parent_class)->dispose (object);
 }
 
 static void
@@ -145,7 +134,6 @@ epak_pak_class_init (EpakPakClass *klass)
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
   gobject_class->finalize = epak_pak_finalize;
-  gobject_class->dispose = epak_pak_dispose;
   gobject_class->set_property = epak_pak_set_property;
   gobject_class->get_property = epak_pak_get_property;
 
