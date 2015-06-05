4eac20561d6a2b39c94b0f73134483ab67352af6
Will Greenberg <will@endlessm.com>
2015-06-05 10:40:41 -0700

    EosShardBlobStream is Seekable when data is uncompressed
    [endlessm/eos-sdk#3248]

---
+++ test/streamSpec.js
@@ -0,0 +1,75 @@
+/* Copyright 2015 Endless Mobile, Inc. */
+
+/* This file is part of eos-shard.
+ *
+ * eos-shard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * eos-shard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with eos-shard.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
+
+const EosShard = imports.gi.EosShard;
+const TestUtils = imports.utils;
+
+describe('EosShardBlobStream', function () {
+    afterEach(function () {
+        let file = Gio.File.new_for_path('test.shard');
+        file.delete(null);
+    });
+
+    beforeEach(function () {
+        let shard_writer = new EosShard.Writer();
+
+        shard_writer.add_record('7d97e98f8af710c7e7fe703abc8f639e0ee507c4');
+        shard_writer.add_blob(EosShard.WriterBlob.METADATA,
+                              TestUtils.getTestFile('7d97e98f8af710c7e7fe703abc8f639e0ee507c4.json'),
+                              'application/json',
+                              EosShard.BlobFlags.COMPRESSED_ZLIB);
+        shard_writer.add_blob(EosShard.WriterBlob.DATA,
+                              TestUtils.getTestFile('7d97e98f8af710c7e7fe703abc8f639e0ee507c4.blob'),
+                              null,
+                              EosShard.BlobFlags.COMPRESSED_ZLIB);
+
+        shard_writer.add_record('f572d396fae9206628714fb2ce00f72e94f2258f');
+        shard_writer.add_blob(EosShard.WriterBlob.METADATA,
+                              TestUtils.getTestFile('f572d396fae9206628714fb2ce00f72e94f2258f.json'),
+                              'application/json',
+                              EosShard.BlobFlags.COMPRESSED_ZLIB);
+        shard_writer.add_blob(EosShard.WriterBlob.DATA,
+                              TestUtils.getTestFile('f572d396fae9206628714fb2ce00f72e94f2258f.blob'),
+                              null,
+                              EosShard.BlobFlags.NONE);
+
+        shard_writer.write('test.shard');
+    });
+
+    it('should be seekable when uncompressed', function () {
+        let shard_file = new EosShard.ShardFile({ path: 'test.shard' });
+        shard_file.init(null);
+
+        let record = shard_file.find_record_by_hex_name('f572d396fae9206628714fb2ce00f72e94f2258f');
+        let dataStream = record.data.get_stream();
+        expect(GObject.type_is_a(dataStream, Gio.Seekable)).toBe(true);
+    });
+
+    it('should not be seekable when compressed', function () {
+        let shard_file = new EosShard.ShardFile({ path: 'test.shard' });
+        shard_file.init(null);
+
+        let record = shard_file.find_record_by_hex_name('7d97e98f8af710c7e7fe703abc8f639e0ee507c4');
+        let dataStream = record.data.get_stream();
+        expect(GObject.type_is_a(dataStream, Gio.Seekable)).toBe(false);
+    });
+});
