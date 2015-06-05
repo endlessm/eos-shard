480c8fb50c0c7bb20f27d98087a340c0c54dc9fc
Will Greenberg <will@endlessm.com>
2015-06-05 10:40:04 -0700

    refactor: extract test utils to test/utils.js
    [endlessm/eos-sdk#3248]

---
+++ test/utils.js
@@ -0,0 +1,28 @@
+/* Copyright 2015 Endless Mobile, Inc. */
+
+/* This file is part of eos-shard.
+ *
+ * eos-shard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * eos-shard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with eos-shard.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+
+function getTestFile(fn) {
+    let datadir = GLib.getenv('G_TEST_SRCDIR');
+    if (!datadir)
+        datadir = '';
+    return Gio.File.new_for_path(GLib.build_filenamev([datadir, 'test/data', fn]));
+}
