fc6074b7d6eb80737565606fec092349e39d8bea
Will Greenberg <will@endlessm.com>
2016-04-29 14:23:32 -0700

    wip

--- write.py
+++ write.py
@@ -15,15 +15,13 @@ def rot13(s):
     return string.translate(s, t)
 
 words = [w.rstrip().lower() for w in open('/usr/share/dict/words')]
-words = sorted(words)[:10]
+words = sorted(words)
 
 def write_jlist():
     writer = EosShard.Writer()
     at = EosShard.AliasTable.new(len(words))
-    for i, word in enumerate(words):
-        key = word
-        value = rot13(key)
-        at.add_entry(key, value)
+    for word in words:
+        at.add_entry(word, rot13(word))
     at.write_to_shard(writer)
     writer.write('foo.shard')
 
@@ -32,9 +30,8 @@ def test():
     shard.init(None)
     at = EosShard.AliasTable.new_from_shard(shard)
     results = at.find_entries(words)
-    for word, (key, value) in zip(words, sorted(results.iteritems())):
-        assert key == word
-        assert(rot13(key) == value)
+    for word, value in results.iteritems():
+        assert(value == rot13(word))
 
 write_jlist()
 test()
