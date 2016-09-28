860460214c7eae39140e96a474608ff752c240a8
Will Greenberg <will@endlessm.com>
2016-09-28 16:11:36 -0700

    blob: make eos_shard_blob_get_packed_size public
    This method is useful for introspecting the on-disk use of space.

--- src/eos-shard-blob.h
+++ src/eos-shard-blob.h
@@ -69,7 +69,7 @@ gsize eos_shard_blob_get_content_size (EosShardBlob *blob);
 EosShardBlob * eos_shard_blob_ref (EosShardBlob *blob);
 void eos_shard_blob_unref (EosShardBlob *blob);
 
-gsize _eos_shard_blob_get_packed_size (EosShardBlob *blob);
+gsize eos_shard_blob_get_packed_size (EosShardBlob *blob);
 goffset eos_shard_blob_get_offset (EosShardBlob *blob);
 
 EosShardDictionary * eos_shard_blob_load_as_dictionary (EosShardBlob *blob, GError **error);
