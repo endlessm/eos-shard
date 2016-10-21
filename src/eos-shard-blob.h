5561baecc158e6fe37ca72ea915b27140502326c
Will Greenberg <will@endlessm.com>
2016-10-21 10:10:34 -0700

    Revert "eos-shard-blob: add get_blob_size method"
    This'll be replaced with get_packed_size

--- src/eos-shard-blob.h
+++ src/eos-shard-blob.h
@@ -52,7 +52,6 @@ struct _EosShardBlob {
   char *content_type;
   EosShardBlobFlags flags;
   uint8_t checksum[0x20];
-  uint64_t hdr_size;
   uint64_t offs;
   uint64_t size;
   uint64_t uncompressed_size;
@@ -69,7 +68,6 @@ EosShardBlobFlags eos_shard_blob_get_flags (EosShardBlob *blob);
 gsize eos_shard_blob_get_content_size (EosShardBlob *blob);
 EosShardBlob * eos_shard_blob_ref (EosShardBlob *blob);
 void eos_shard_blob_unref (EosShardBlob *blob);
-gsize eos_shard_blob_get_blob_size (EosShardBlob *blob);
 
 gsize _eos_shard_blob_get_packed_content_size (EosShardBlob *blob);
 goffset eos_shard_blob_get_offset (EosShardBlob *blob);
