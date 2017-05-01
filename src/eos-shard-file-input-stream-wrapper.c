3e052c275cbe2f4fa3681b294d7425b20a267003
Sam Spilsbury <sam@endlessm.com>
2017-05-01 15:11:01 +0800

    shard: Add implementation of EosShardFile from Eknc
    We are moving the implementation here so that we can share this
    code with system components without it having to depend on
    Eknc directly.

---
+++ src/eos-shard-file-input-stream-wrapper.c
@@ -0,0 +1,260 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * eos-shard-file-input-stream-wrapper.c
+ *
+ * Copyright (C) 2016 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "eos-shard-file-input-stream-wrapper.h"
+
+struct _EosShardFileInputStreamWrapper
+{
+  GFileInputStream parent;
+
+  GInputStream *stream;
+  GFile *file;
+};
+typedef struct _EosShardFileInputStreamWrapper EosShardFileInputStreamWrapper;
+
+enum
+{
+  PROP_0,
+  PROP_STREAM,
+  PROP_FILE,
+  LAST_PROP,
+};
+
+static GParamSpec *obj_props[LAST_PROP];
+
+G_DEFINE_TYPE (EosShardFileInputStreamWrapper,
+               eos_shard_file_input_stream_wrapper,
+               G_TYPE_FILE_INPUT_STREAM)
+
+static void
+eos_shard_file_input_stream_wrapper_dispose (GObject *object)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (object);
+
+  g_clear_object (&self->stream);
+  g_clear_object (&self->file);
+
+  G_OBJECT_CLASS (eos_shard_file_input_stream_wrapper_parent_class)->dispose (object);
+}
+
+static void
+eos_shard_file_input_stream_wrapper_set_property (GObject          *object,
+                                                  guint             prop_id,
+                                                  const GValue     *value,
+                                                  GParamSpec       *pspec)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (object);
+
+  switch (prop_id)
+    {
+    case PROP_STREAM:
+      self->stream = g_value_dup_object (value);
+      break;
+    case PROP_FILE:
+      self->file = g_value_dup_object (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+eos_shard_file_input_stream_wrapper_get_property (GObject    *object,
+                                                  guint       prop_id,
+                                                  GValue     *value,
+                                                  GParamSpec *pspec)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (object);
+
+  switch (prop_id)
+    {
+    case PROP_STREAM:
+      g_value_set_object (value, self->stream);
+      break;
+    case PROP_FILE:
+      g_value_set_object (value, self->file);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+#define return_error_if_fail(expr,code,msg,val) \
+if (!(expr)) \
+  { \
+    g_warning ("%s "#expr" failed", __func__); \
+    g_set_error_literal (error, G_IO_ERROR, code, msg); \
+    return val;\
+  }\
+
+#define return_error_if_no_stream(val) \
+  return_error_if_fail (self->stream, G_IO_ERROR_FAILED, "No input stream to wrap", val)
+
+static gssize
+eos_shard_file_input_stream_wrapper_read (GInputStream  *stream,
+                                          void          *buffer,
+                                          gsize          count,
+                                          GCancellable  *cancellable,
+                                          GError       **error)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (stream);
+
+  return_error_if_no_stream (-1);
+
+  return g_input_stream_read (self->stream, buffer, count, cancellable, error);
+}
+
+static gboolean
+eos_shard_file_input_stream_wrapper_close (GInputStream  *stream,
+                                           GCancellable  *cancellable,
+                                           GError       **error)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (stream);
+
+  /* This happens on dispose */
+  if (!self->stream)
+    {
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_CLOSED, "Stream already closed");
+      return FALSE;
+    }
+
+  return g_input_stream_close (self->stream, cancellable, error);
+}
+
+static gssize
+eos_shard_file_input_stream_wrapper_skip (GInputStream  *stream,
+                                          gsize          count,
+                                          GCancellable  *cancellable,
+                                          GError       **error)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (stream);
+
+  return_error_if_no_stream (-1);
+
+  return g_input_stream_skip (self->stream, count, cancellable, error);
+}
+
+static goffset
+eos_shard_file_input_stream_wrapper_tell (GFileInputStream *stream)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (stream);
+
+  g_return_val_if_fail (self->stream, 0);
+
+  if (!G_IS_SEEKABLE (self->stream))
+    return 0;
+
+  return g_seekable_tell (G_SEEKABLE (self->stream));
+}
+
+static gboolean
+eos_shard_file_input_stream_wrapper_can_seek (GFileInputStream *stream)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (stream);
+
+  g_return_val_if_fail (self->stream, FALSE);
+
+  return G_IS_SEEKABLE (self->stream) && g_seekable_can_seek (G_SEEKABLE (self->stream));
+}
+
+static gboolean
+eos_shard_file_input_stream_wrapper_seek (GFileInputStream *stream,
+                                          goffset offset,
+                                          GSeekType type,
+                                          GCancellable *cancellable,
+                                          GError **error)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (stream);
+
+  return_error_if_no_stream (FALSE);
+  return_error_if_fail (G_IS_SEEKABLE (self->stream), G_IO_ERROR_NOT_SUPPORTED,
+                        "Input stream doesn't implement seek", FALSE);
+
+  return g_seekable_seek (G_SEEKABLE (self->stream), offset, type, cancellable, error);
+}
+
+static GFileInfo *
+eos_shard_file_input_stream_wrapper_query_info (GFileInputStream  *stream,
+                                                const char        *attributes,
+                                                GCancellable      *cancellable,
+                                                GError           **error)
+{
+  EosShardFileInputStreamWrapper *self = EOS_SHARD_FILE_INPUT_STREAM_WRAPPER (stream);
+
+  return_error_if_fail (self->file, G_IO_ERROR_FAILED, "No GFile to wrap", NULL);
+
+  return g_file_query_info (self->file, attributes, 0, cancellable, error);
+}
+
+static void
+eos_shard_file_input_stream_wrapper_class_init (EosShardFileInputStreamWrapperClass *klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+  GInputStreamClass *istream_class;
+  GFileInputStreamClass *fileistream_klass;
+
+  gobject_class->dispose = eos_shard_file_input_stream_wrapper_dispose;
+  gobject_class->set_property = eos_shard_file_input_stream_wrapper_set_property;
+  gobject_class->get_property = eos_shard_file_input_stream_wrapper_get_property;
+
+  istream_class = G_INPUT_STREAM_CLASS (klass);
+  istream_class->read_fn = eos_shard_file_input_stream_wrapper_read;
+  istream_class->close_fn = eos_shard_file_input_stream_wrapper_close;
+  istream_class->skip = eos_shard_file_input_stream_wrapper_skip;
+
+  fileistream_klass = G_FILE_INPUT_STREAM_CLASS (klass);
+  fileistream_klass->tell = eos_shard_file_input_stream_wrapper_tell;
+  fileistream_klass->can_seek = eos_shard_file_input_stream_wrapper_can_seek;
+  fileistream_klass->seek = eos_shard_file_input_stream_wrapper_seek;
+  fileistream_klass->query_info = eos_shard_file_input_stream_wrapper_query_info;
+
+  obj_props[PROP_STREAM] =
+    g_param_spec_object ("stream", "", "",
+                         G_TYPE_INPUT_STREAM,
+                         (GParamFlags) (G_PARAM_READWRITE |
+                                        G_PARAM_CONSTRUCT_ONLY |
+                                        G_PARAM_STATIC_STRINGS));
+  obj_props[PROP_FILE] =
+    g_param_spec_object ("file", "", "",
+                         G_TYPE_OBJECT,
+                         (GParamFlags) (G_PARAM_READWRITE |
+                                        G_PARAM_CONSTRUCT_ONLY |
+                                        G_PARAM_STATIC_STRINGS));
+  g_object_class_install_properties (gobject_class, LAST_PROP, obj_props);
+}
+
+static void
+eos_shard_file_input_stream_wrapper_init (EosShardFileInputStreamWrapper *wrapper)
+{
+}
+
+GFileInputStream *
+_eos_shard_file_input_stream_wrapper_new (GFile *file, GInputStream *stream)
+{
+  g_return_val_if_fail (G_IS_FILE (file), NULL);
+  g_return_val_if_fail (G_IS_INPUT_STREAM (stream), NULL);
+
+  return g_object_new (EOS_SHARD_TYPE_FILE_INPUT_STREAM_WRAPPER,
+                       "file", file,
+                       "stream", stream,
+                       NULL);
+}
---
+++ src/eos-shard-file.h
@@ -0,0 +1,47 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * ekn-file.h
+ *
+ * Copyright (C) 2016 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author: Juan Pablo Ugarte <ugarte@endlessm.com>
+ *
+ */
+
+#pragma once
+
+#include <gio/gio.h>
+#include "eos-shard-blob.h"
+
+G_BEGIN_DECLS
+
+#define EOS_SHARD_TYPE_FILE (eos_shard_file_get_type ())
+G_DECLARE_FINAL_TYPE (EosShardFile, eos_shard_file, EOS_SHARD, FILE, GObject)
+
+/**
+ * eos_shard_file_new:
+ * @uri: The URI to create this #GFile at.
+ * @blob: An #EosShardBlob to read for this file (transfer full)
+ *
+ * Creates a new #GFile with support for reading #EosShardBlob
+ * with the URI of uri.
+ *
+ * Returns: (transfer full): A #GFile implementation to read the shard
+ */
+GFile *eos_shard_file_new (const gchar *uri, EosShardBlob *blob);
+
+G_END_DECLS
